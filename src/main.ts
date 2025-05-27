69944da40f98b93c5b559b86001ca31f2f21b8ed
echimine <eliott.chiminello@gmail.com>
2025-05-27 13:27:44 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = process.env.PORT ?? 3002;
   await app.listen(port);
   console.log(`🚀 App is running on http://localhost:${port}`);
